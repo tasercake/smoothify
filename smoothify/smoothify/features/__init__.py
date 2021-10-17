b1e9b89d75f150ffcb569c68999cf49fa0c63a00
Krishna <penukonda.krishna.moorthy@gmail.com>
2021-10-17 22:11:34 +0800

    Made audio_analysis_list optional

--- smoothify/smoothify/features/__init__.py
+++ smoothify/smoothify/features/__init__.py
@@ -1,4 +1,6 @@
 from typing import Any, List, Dict
+from itertools import zip_longest
+
 import pandas as pd
 
 Track = Dict[str, Any]
@@ -8,17 +10,13 @@ AudioAnalysis = Dict[str, Any]
 
 def construct_features(
     *,
-    track_list: List[Track],
     audio_features_list: List[AudioFeatures],
-    audio_analysis_list: List[AudioAnalysis],
+    audio_analysis_list: List[AudioAnalysis] = None,
     normalize: bool = True,
 ) -> pd.DataFrame:
+    audio_analysis_list = audio_analysis_list or []
     features_list = []
 
-    if not len(track_list) == len(audio_features_list) == len(audio_analysis_list):
-        raise ValueError("Sequence lengths must match.")
-
-    track_keys = ("popularity",)
     audio_features_keys = (
         "danceability",
         "energy",
@@ -32,21 +30,22 @@ def construct_features(
     )
     audio_analysis_track_keys = ("mode",)
 
-    for track, audio_features, audio_analysis in zip(
-        track_list, audio_features_list, audio_analysis_list
+    for audio_features, audio_analysis in zip_longest(
+        audio_features_list, audio_analysis_list
     ):
         features = {}
-        features.update({key: track[key] for key in track_keys})
 
         features.update({key: audio_features[key] for key in audio_features_keys})
 
-        audio_analysis_track_features = audio_analysis["track"]
-        features.update(
-            {
-                key: audio_analysis_track_features[key]
-                for key in audio_analysis_track_keys
-            }
-        )
+        if audio_analysis:
+            audio_analysis_track_features = audio_analysis["track"]
+            features.update(
+                {
+                    key: audio_analysis_track_features[key]
+                    for key in audio_analysis_track_keys
+                }
+            )
+
         features_list.append(features)
 
     # Convert to dataframe
