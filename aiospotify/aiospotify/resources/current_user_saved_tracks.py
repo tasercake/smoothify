7fb92c172bdca531ad7de0ae8aab08b1deac4599
Krishna <penukonda.krishna.moorthy@gmail.com>
2021-11-08 03:27:20 +0800

    Moved resources to sub-packages

--- aiospotify/aiospotify/resources/current_user_saved_tracks.py
+++
@@ -1,9 +0,0 @@
-from aiospotify.resources.abstract.listable_resource import ListableResource
-from aiospotify.models.abstract.spotify_paging_object import SpotifyPagingObject
-from aiospotify.models.spotify_saved_track import SavedTrack
-
-
-class CurrentUserSavedTracks(ListableResource[SavedTrack]):
-    path = "/me/tracks"
-    response_type = SpotifyPagingObject[SavedTrack]
-    max_limit = 50
