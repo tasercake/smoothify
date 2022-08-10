f2024d4bf6e9184d37cf6d9bfc20857be7370bc1
Krishna Penukonda <krishna@penukonda.me>
2022-08-10 17:25:40 +0800

    Moved shortest path computation to webWorker

--- frontend/lib/graphUtils.tsx
+++ frontend/lib/graphUtils.tsx
@@ -1,3 +1,46 @@
-export const shortestPath = () => {
-  // Placeholder for shortest path computation
-};
+/**
+ * Compute pairwise distances using geometric mean of distances along each coordinate
+ */
+export const distanceFn = (p1: number[], p2: number[]) => {
+  // distance along each axis between each coordinate
+  const diffs = p1.map((c, k) => Math.abs(c - p2[k]))
+  // geometric mean of diffs
+  return Math.pow(
+    diffs.reduce((a, b) => a * b),
+    1 / diffs.length
+  )
+}
+
+export const singleSourceBestPath = (points: number[][]) => {
+  const distances = points.map((p1, i) =>
+    points.map((p2, j) => (i === j ? 0 : distanceFn(p1, p2)))
+  )
+  const distancesWithIndices = distances.map((row) =>
+    row.map((d, i) => [i, d] as const)
+  )
+  const sortedDistances = distancesWithIndices.map((row) =>
+    row.sort(([_, a], [__, b]) => a - b)
+  )
+  const nearestNeighbors = sortedDistances.map((row) =>
+    row.slice(1).map(([i, _]) => i)
+  )
+
+  const visited = new Set<number>()
+  const bestPath = []
+
+  let currentIdx = 0 // TODO: Start with a random index
+  visited.add(currentIdx)
+  bestPath.push(currentIdx)
+
+  while (visited.size < points.length) {
+    const nextIdx = nearestNeighbors[currentIdx].find((i) => !visited.has(i))
+    if (nextIdx === undefined) {
+      break
+    }
+    visited.add(nextIdx)
+    bestPath.push(nextIdx)
+    currentIdx = nextIdx
+  }
+
+  return bestPath.map((i) => points[i])
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,52 +1,5 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
-
-/**
- * Compute pairwise distances using geometric mean of distances along each coordinate
- */
-const distanceFn = (p1: number[], p2: number[]) => {
-  // distance along each axis between each coordinate
-  const diffs = p1.map((c, k) => Math.abs(c - p2[k]))
-  // geometric mean of diffs
-  return Math.pow(
-    diffs.reduce((a, b) => a * b),
-    1 / diffs.length
-  )
-}
-
-const getBestPath = (points: number[][]) => {
-  const distances = points.map((p1, i) =>
-    points.map((p2, j) => (i === j ? 0 : distanceFn(p1, p2)))
-  )
-  const distancesWithIndices = distances.map((row) =>
-    row.map((d, i) => [i, d] as const)
-  )
-  const sortedDistances = distancesWithIndices.map((row) =>
-    row.sort(([_, a], [__, b]) => a - b)
-  )
-  const nearestNeighbors = sortedDistances.map((row) =>
-    row.slice(1).map(([i, _]) => i)
-  )
-
-  const visited = new Set<number>()
-  const bestPath = []
-
-  let currentIdx = 0 // TODO: Start with a random index
-  visited.add(currentIdx)
-  bestPath.push(currentIdx)
-
-  while (visited.size < points.length) {
-    const nextIdx = nearestNeighbors[currentIdx].find((i) => !visited.has(i))
-    if (nextIdx === undefined) {
-      break
-    }
-    visited.add(nextIdx)
-    bestPath.push(nextIdx)
-    currentIdx = nextIdx
-  }
-
-  return bestPath.map((i) => points[i])
-}
+import { useEffect, useRef, useState } from 'react'
 
 const generateRandomPoints = (nPoints: number, nDims: number) => {
   return Array.from({ length: nPoints }, () =>
@@ -60,6 +13,18 @@ const N_DIMS = 20
 const Home = () => {
   const [points, setPoints] = useState<number[][] | undefined>()
   const [bestPath, setBestPath] = useState<number[][] | undefined>(undefined)
+  const workerRef = useRef<Worker>()
+
+  useEffect(() => {
+    const worker = new Worker(
+      new URL('../lib/webWorkers/singleSourceBestPath.ts', import.meta.url)
+    )
+    workerRef.current = worker
+    worker.onmessage = (event) => {
+      console.log('Main thread received event', event.data)
+      setBestPath(event.data)
+    }
+  }, [])
 
   return (
     <div>
@@ -84,11 +49,7 @@ const Home = () => {
                 <li key={point.join(' ')}>{point.join(' ')}</li>
               ))}
             </ol>
-            <button
-              onClick={() => {
-                setBestPath(getBestPath(points))
-              }}
-            >
+            <button onClick={() => workerRef.current?.postMessage(points)}>
               Sort points
             </button>
             <p>Sorted</p>
